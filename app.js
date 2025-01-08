8d48187f68c4437666de5ad9cb96a907588b7a78
YousifYA-2366647 <yousif.abbas@UHasselt.student.be>
2025-01-08 17:55:59 +0100

    again, minor improvements. added perks and equipment,...

--- app.js
+++ app.js
@@ -5,6 +5,125 @@ const express = require('express');
 const app = express();
 const APP_PORT = 8080;
 
+function getWeaponsFromCSV(fileName) {
+  weapons = {};
+  curWeapon = "";
+
+  const fileContent = fs.readFileSync(fileName, 'utf-8');
+  fileContent.split("\n").forEach(line => {
+    line = line.trim().replace(/\n/g, '')
+    let records = line.split(";");
+    if (records.length != 0) {
+      if (records[0] == "Name") {
+        weapons[records[1]] = {};
+        curWeapon = records[1];
+      }
+      else {
+        var randomAttachment = records.slice(1, records.length)[Math.floor(Math.random()*records.length)];
+        weapons[curWeapon][records[0]] = randomAttachment;
+      }
+    }
+  })
+
+  return weapons;
+}
+
+function getEquipmentFromCSV(fileName) {
+  equipment = [];
+
+  const fileContent = fs.readFileSync(fileName, 'utf-8');
+  fileContent.split("\n").forEach(line => {
+    line = line.trim().replace(/\n/g, '')
+    let records = line.split(";");
+    if (records.length != 0) {
+      if (records[0] == "Name") {
+        equipment.push(records[1]);
+      }
+    }
+  })
+
+  return equipment;
+}
+
+function getRandomItemsFromJSON(JsonObject) {
+  let randomEntry = Object.entries(JsonObject)[Math.floor(Math.random()*Object.keys(JsonObject).length)];
+
+  let randomKey = randomEntry[0];
+  let randomJson = {};
+  randomJson[randomKey] = randomEntry[1];
+
+  return randomJson;
+}
+
+function getRandomWildcard(arrayObject) {
+  let randomEntry = arrayObject[Math.floor(Math.random()*arrayObject.length)];
+
+  return randomEntry;
+}
+
+function getRandomElements(arrayObject, amount) {
+  let chosenItems = [];
+
+  for (let i = 0; i < amount; i++) {
+    let randomObject = arrayObject[Math.floor(Math.random()*arrayObject.length)];
+    chosenItems.push(randomObject);
+    arrayObject.splice(arrayObject.indexOf(randomObject), 1);
+  }
+
+  return chosenItems;
+}
+
+function getRandomAttachments(weaponJson, amount) {
+  let weaponName = Object.keys(weaponJson)[0];
+  let attachments = Object.values(weaponJson)[0];
+  let attachmentKeys = Object.keys(attachments);
+
+  let iterateLength = attachmentKeys.length - amount;
+
+  for (const key in attachments) {
+    if (attachments[key] == "") {
+      iterateLength--;
+    }
+  }
+
+  for (let i = 0; i < iterateLength; i++) {
+    let randomAttachmentKey = attachmentKeys[Math.floor(Math.random()*attachmentKeys.length)];
+    weaponJson[weaponName][randomAttachmentKey] = "";
+  }
+
+  return weaponJson;
+}
+
+function addSpecialPerk(perks, enforcer, recon, specialist) {
+  let amountOfE = 0;
+  let amountOfR = 0;
+  let amountOfS = 0;
+
+  for (const perk in perks) {
+    if (enforcer.indexOf(perks[perk]) != -1) {
+      amountOfE++;
+    }
+    else if (recon.indexOf(perks[perk]) != -1) {
+      amountOfR++;
+    }
+    else if (specialist.indexOf(perks[perk]) != -1) {
+      amountOfS++;
+    }
+  }
+
+  if (amountOfE >= 3) {
+    perks.push("Enforcer");
+  }
+  else if (amountOfR >= 3) {
+    perks.push("Recon");
+  }
+  else if (amountOfS >= 3) {
+    perks.push("Specialist");
+  }
+
+  return perks;
+}
+
 app.set('view engine', 'ejs');
 
 app.get("/", (request, response) => {
@@ -19,81 +138,64 @@ app.get("/getWeapons", (request, response) => {
   let primaries = {};
   let secondaries = {};
   let melees = {};
-
-  let curPrimary = "";
-  let curSecondary = "";
-  let curMelee = "";
+  let lethals = getEquipmentFromCSV("Weapon_data/Lethal_data.csv");
+  let tacticals = getEquipmentFromCSV("Weapon_data/Tactical_data.csv");
+  let enforcerPerks = getEquipmentFromCSV("Weapon_data/EnforcerPerk_data.csv");
+  let reconPerks = getEquipmentFromCSV("Weapon_data/ReconPerk_data.csv");
+  let specialistPerks = getEquipmentFromCSV("Weapon_data/SpecialistPerk_data.csv");
+  let wildcards = getEquipmentFromCSV("Weapon_data/Wildcard_data.csv");
+  let scorestreaks = getEquipmentFromCSV("Weapon_data/Scorestreak_data.csv");
+  let fieldUpgrades = getEquipmentFromCSV("Weapon_data/FieldUpgrade_data.csv");
 
   let primaryFiles = ["Weapon_data/AR_data.csv", "Weapon_data/SMG_data.csv", "Weapon_data/Shotgun_data.csv", "Weapon_data/LMG_data.csv", "Weapon_data/MarksmanRifle_data.csv", "Weapon_data/SniperRifle_data.csv"];
   let secondaryFiles = ["Weapon_data/Pistol_data.csv", "Weapon_data/Launcher_data.csv"];
   primaryFiles.forEach((file) => {
-    const fileContent = fs.readFileSync(file, 'utf-8');
-    fileContent.split("\n").forEach(line => {
-      line = line.trim().replace(/\n/g, '')
-      let records = line.split(";");
-      if (records.length != 0) {
-        if (records[0] == "Name") {
-          primaries[records[1]] = {};
-          curPrimary = records[1];
-        }
-        else {
-          var randomAttachment = records.slice(1, records.length)[Math.floor(Math.random()*records.length)];
-          primaries[curPrimary][records[0]] = randomAttachment;
-        }
-      }
-    })
+    Object.assign(primaries, getWeaponsFromCSV(file));
   })
   secondaryFiles.forEach((file) => {
-    const fileContent = fs.readFileSync(file, 'utf-8');
-    fileContent.split("\n").forEach(line => {
-      line = line.trim().replace(/\n/g, '')
-      let records = line.split(";");
-      if (records.length != 0) {
-        if (records[0] == "Name") {
-          secondaries[records[1]] = {};
-          curSecondary = records[1];
-        }
-        else {
-          var randomAttachment = records.slice(1, records.length)[Math.floor(Math.random()*records.length)];
-          secondaries[curSecondary][records[0]] = randomAttachment;
-        }
-      }
-    })
+    Object.assign(secondaries, getWeaponsFromCSV(file));
   })
 
-  const fileContent = fs.readFileSync("Weapon_data/Melee_data.csv", 'utf-8');
-    fileContent.split("\n").forEach(line => {
-      line = line.trim().replace(/\n/g, '')
-      let records = line.split(";");
-      if (records.length != 0) {
-        if (records[0] == "Name") {
-          melees[records[1]] = {};
-          curMelee = records[1];
-        }
-        else {
-          var randomAttachment = records.slice(1, records.length)[Math.floor(Math.random()*records.length)];
-          melees[curMelee][records[0]] = randomAttachment;
-        }
-      }
-    })
+  Object.assign(melees, getWeaponsFromCSV("Weapon_data/Melee_data.csv"));
 
-  let randomPrimary = Object.entries(primaries)[Math.floor(Math.random()*Object.keys(primaries).length)];
-  let randomSecondary = Object.entries(secondaries)[Math.floor(Math.random()*Object.keys(secondaries).length)];
-  let randomMelee = Object.entries(melees)[Math.floor(Math.random()*Object.keys(melees).length)];
+  let randomPrimary = getRandomItemsFromJSON(primaries);
+  let randomSecondary = getRandomItemsFromJSON(secondaries);
+  let randomMelee = getRandomItemsFromJSON(melees);
 
-  let primaryKey = randomPrimary[0];
-  let primaryJson = {};
-  primaryJson[primaryKey] = randomPrimary[1];
+  let randomWildcard = getRandomWildcard(wildcards);
 
-  let secondaryKey = randomSecondary[0];
-  let secondaryJson = {};
-  secondaryJson[secondaryKey] = randomSecondary[1];
+  let amountOfLethals = (randomWildcard == "Danger Close") ? 2: 1;
+  let randomLethal = getRandomElements([...lethals], 1);
+  if (amountOfLethals == 2) {
+    randomLethal = [...randomLethal, ...randomLethal];
+  }
 
-  let meleeKey = randomMelee[0];
-  let meleeJson = {};
-  meleeJson[meleeKey] = randomMelee[1];
+  let amountOfTacticals = (randomWildcard == "Tactical Expert") ? 3: 1;
+  let randomTactical = getRandomElements([...tacticals], 1);
+  if (amountOfTacticals == 3) {
+    randomTactical = [...randomTactical, ...randomTactical, ...randomTactical];
+  }
 
-  response.status(200).json({primary: primaryJson, secondary: secondaryJson, melee: meleeJson});
+  let amountOfPerks = (randomWildcard == "Perk Greed") ? 4: 3;
+  let randomPerks = getRandomElements([...enforcerPerks, ...reconPerks, ...specialistPerks], amountOfPerks);
+  randomPerks = addSpecialPerk(randomPerks, enforcerPerks, reconPerks, specialistPerks);
+
+  let amountOfStreaks = (randomWildcard == "High Roller") ? 4: 3;
+  let randomScorestreaks = getRandomElements([...scorestreaks], amountOfStreaks);
+
+  let amountOfFieldUpgrades = (randomWildcard == "Prepper") ? 2: 1;
+  let randomFieldUpgrades = getRandomElements([...fieldUpgrades], amountOfFieldUpgrades);
+
+  let amountOfAttachments = (randomWildcard == "Gunfighter") ? 8: 5;
+  randomPrimary = getRandomAttachments({...randomPrimary}, amountOfAttachments);
+
+  if (randomWildcard == "Overkill") {
+    randomSecondary = getRandomItemsFromJSON(primaries);
+  }
+
+  randomSecondary = getRandomAttachments({...randomSecondary}, 5);
+
+  response.status(200).json({primary: randomPrimary, secondary: randomSecondary, melee: randomMelee, lethal: randomLethal, tactical: randomTactical, fieldUpgrade: randomFieldUpgrades, perks: randomPerks, wildcard: randomWildcard, scorestreaks: randomScorestreaks});
 });
 
 app.get("/disclaimer", (request, response) => {
--- public/js/generateRandomClass.js
+++ public/js/generateRandomClass.js
@@ -17,6 +17,116 @@ function getPicturePath(weaponName, attachmentType, attachmentName) {
     return fullPath + ".png";
 }
 
+function setupPrimary(jsonObject) {
+    let primaryAttachements = document.getElementById("primaryAttachments");
+    primaryAttachements.innerHTML = '';
+    document.getElementById("primaryName").innerText = Object.keys(jsonObject)[0];
+    document.getElementById("primaryImage").src = getPicturePath(Object.keys(jsonObject)[0], null, null);
+
+    let chosenAttachements = Object.values(jsonObject)[0];
+    for (const key in chosenAttachements) {
+        let image = document.createElement('img');
+        image.className = "attachement";
+        ImagePath = getPicturePath(Object.keys(jsonObject)[0], key, chosenAttachements[key]);
+        if (ImagePath == -1) {
+            let division = document.createElement('div');
+            division.className = "attachement";
+            primaryAttachements.appendChild(division);
+            continue;
+        }
+        image.src = ImagePath;
+
+        primaryAttachements.appendChild(image);
+    }
+}
+
+function setupSecondary(jsonObject) {
+    let secondaryAttachements = document.getElementById("secondaryAttachments");
+    secondaryAttachements.innerHTML = '';
+    document.getElementById("secondaryName").innerText = Object.keys(jsonObject)[0];
+    document.getElementById("secondaryImage").src = getPicturePath(Object.keys(jsonObject)[0], null, null);
+
+    let chosenAttachementsSecondary = Object.values(jsonObject)[0];
+    for (const key in chosenAttachementsSecondary) {
+        let image = document.createElement('img');
+        image.className = "attachement";
+        ImagePath = getPicturePath(Object.keys(jsonObject)[0], key, chosenAttachementsSecondary[key]);
+        if (ImagePath == -1) {
+            let division = document.createElement('div');
+            division.className = "attachement";
+            secondaryAttachements.appendChild(division);
+            continue;
+        }
+        image.src = ImagePath;
+
+        secondaryAttachements.appendChild(image);
+    }
+}
+
+function setupEquipment(lethalJsonObject, tacticalJsonObject, fieldJsonObject) {
+    let lethalDiv = document.getElementById("randomLethal");
+    let tacticalDiv = document.getElementById("randomTactical");
+    let fieldDiv = document.getElementById("randomFieldUpgrade");
+    lethalDiv.innerHTML = '';
+    tacticalDiv.innerHTML = '';
+    fieldDiv.innerHTML = '';
+
+    for (const key in lethalJsonObject) {
+        let paragraph = document.createElement('p');
+        paragraph.className = "equipment";
+        paragraph.innerText = lethalJsonObject[key];
+
+        lethalDiv.appendChild(paragraph);
+    }
+
+    for (const key in tacticalJsonObject) {
+        let paragraph = document.createElement('p');
+        paragraph.className = "equipment";
+        paragraph.innerText = tacticalJsonObject[key];
+
+        tacticalDiv.appendChild(paragraph);
+    }
+
+    for (const key in fieldJsonObject) {
+        let paragraph = document.createElement('p');
+        paragraph.className = "equipment";
+        paragraph.innerText = fieldJsonObject[key];
+
+        fieldDiv.appendChild(paragraph);
+    }
+}
+
+function setupPerks(perkJsonObject) {
+    let perkDiv = document.getElementById("randomPerks");
+    perkDiv.innerHTML = '';
+
+    for (const key in perkJsonObject) {
+        let paragraph = document.createElement('p');
+        paragraph.className = "perk";
+        paragraph.innerText = perkJsonObject[key];
+
+        perkDiv.appendChild(paragraph);
+    }
+}
+
+function setupWildcard(wildcard) {
+    let wildcardText = document.getElementById("randomWildcardText");
+    wildcardText.innerText = wildcard;
+}
+
+function setupScorestreaks(scorestreakJsonObject) {
+    let scorestreakDiv = document.getElementById("randomScorestreaks");
+    scorestreakDiv.innerHTML = '';
+
+    for (const key in scorestreakJsonObject) {
+        let paragraph = document.createElement('p');
+        paragraph.className = "scorestreak";
+        paragraph.innerText = scorestreakJsonObject[key];
+
+        scorestreakDiv.appendChild(paragraph);
+    }
+}
+
 function getRandomWeapons() {
     fetch("/getWeapons", {
         method: "GET"
@@ -30,50 +140,20 @@ function getRandomWeapons() {
     .then(res => {
         console.log(JSON.stringify(res));
 
-        let primaryAttachements = document.getElementById("primaryAttachments");
-        primaryAttachements.innerHTML = '';
-        document.getElementById("primaryName").innerText = Object.keys(res.primary)[0];
-        document.getElementById("primaryImage").src = getPicturePath(Object.keys(res.primary)[0], null, null);
+        setupPrimary(res.primary);
 
-        let chosenAttachements = Object.values(res.primary)[0];
-        for (const key in chosenAttachements) {
-            let image = document.createElement('img');
-            image.className = "attachement";
-            ImagePath = getPicturePath(Object.keys(res.primary)[0], key, chosenAttachements[key]);
-            if (ImagePath == -1) {
-                let division = document.createElement('div');
-                division.className = "attachement";
-                primaryAttachements.appendChild(division);
-                continue;
-            }
-            image.src = ImagePath;
-
-            primaryAttachements.appendChild(image);
-        }
-
-        let secondaryAttachements = document.getElementById("secondaryAttachments");
-        secondaryAttachements.innerHTML = '';
-        document.getElementById("secondaryName").innerText = Object.keys(res.secondary)[0];
-        document.getElementById("secondaryImage").src = getPicturePath(Object.keys(res.secondary)[0], null, null);
-
-        let chosenAttachementsSecondary = Object.values(res.secondary)[0];
-        for (const key in chosenAttachementsSecondary) {
-            let image = document.createElement('img');
-            image.className = "attachement";
-            ImagePath = getPicturePath(Object.keys(res.secondary)[0], key, chosenAttachementsSecondary[key]);
-            if (ImagePath == -1) {
-                let division = document.createElement('div');
-                division.className = "attachement";
-                secondaryAttachements.appendChild(division);
-                continue;
-            }
-            image.src = ImagePath;
-
-            secondaryAttachements.appendChild(image);
-        }
+        setupSecondary(res.secondary);
 
         document.getElementById("meleeName").innerText = Object.keys(res.melee)[0];
         document.getElementById("meleeImage").src = getPicturePath(Object.keys(res.melee)[0], null, null);
+
+        setupEquipment(res.lethal, res.tactical, res.fieldUpgrade);
+
+        setupPerks(res.perks);
+
+        setupWildcard(res.wildcard);
+
+        setupScorestreaks(res.scorestreaks);
     })
 }
 
