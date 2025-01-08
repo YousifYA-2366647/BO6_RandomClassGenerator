2826dcbb92d1fa9f7f73ef597958655513b8bc7c
YousifYA-2366647 <yousif.abbas@UHasselt.student.be>
2025-01-08 12:28:39 +0100

    added images to the tool

--- public/js/generateRandomClass.js
+++ public/js/generateRandomClass.js
@@ -1,3 +1,21 @@
+function getPicturePath(weaponName, attachmentType, attachmentName) {
+    fullPath = "Resources/" + weaponName; 
+    if (attachmentType != null) {
+        fullPath += "/" + attachmentType 
+    }
+    if (attachmentName != null) {
+        if (attachmentName == "") {
+            return -1;
+        }
+        noSymbolsAttachmentName = attachmentName.replace(/\W/g, '');
+        fullPath += "/" + noSymbolsAttachmentName;
+    }
+    if (attachmentName == null && attachmentType == null) {
+        fullPath += "/" + weaponName;
+    }
+    return fullPath + ".png";
+}
+
 function getRandomWeapons() {
     fetch("/getWeapons", {
         method: "GET"
@@ -9,31 +27,52 @@ function getRandomWeapons() {
         return res.json();
     })
     .then(res => {
+        console.log(JSON.stringify(res));
+
         let primaryAttachements = document.getElementById("primaryAttachments");
         primaryAttachements.innerHTML = '';
         document.getElementById("primaryName").innerText = Object.keys(res.primary)[0];
+        document.getElementById("primaryImage").src = getPicturePath(Object.keys(res.primary)[0], null, null);
+
         let chosenAttachements = Object.values(res.primary)[0];
         for (const key in chosenAttachements) {
-            let paragraph = document.createElement('p');
-            paragraph.className = "attachement";
-            paragraph.innerText = key + ": " + chosenAttachements[key];
+            let image = document.createElement('img');
+            image.className = "attachement";
+            ImagePath = getPicturePath(Object.keys(res.primary)[0], key, chosenAttachements[key]);
+            if (ImagePath == -1) {
+                let division = document.createElement('div');
+                division.className = "attachement";
+                primaryAttachements.appendChild(division);
+                continue;
+            }
+            image.src = ImagePath;
 
-            primaryAttachements.appendChild(paragraph);
+            primaryAttachements.appendChild(image);
         }
 
         let secondaryAttachements = document.getElementById("secondaryAttachments");
         secondaryAttachements.innerHTML = '';
         document.getElementById("secondaryName").innerText = Object.keys(res.secondary)[0];
+        document.getElementById("secondaryImage").src = getPicturePath(Object.keys(res.secondary)[0], null, null);
+
         let chosenAttachementsSecondary = Object.values(res.secondary)[0];
         for (const key in chosenAttachementsSecondary) {
-            let paragraph = document.createElement('p');
-            paragraph.className = "attachement";
-            paragraph.innerText = key + ": " + chosenAttachementsSecondary[key];
+            let image = document.createElement('img');
+            image.className = "attachement";
+            ImagePath = getPicturePath(Object.keys(res.secondary)[0], key, chosenAttachementsSecondary[key]);
+            if (ImagePath == -1) {
+                let division = document.createElement('div');
+                division.className = "attachement";
+                secondaryAttachements.appendChild(division);
+                continue;
+            }
+            image.src = ImagePath;
 
-            secondaryAttachements.appendChild(paragraph);
+            secondaryAttachements.appendChild(image);
         }
 
         document.getElementById("meleeName").innerText = Object.keys(res.melee)[0];
+        document.getElementById("meleeImage").src = getPicturePath(Object.keys(res.melee)[0], null, null);
     })
 }
 
